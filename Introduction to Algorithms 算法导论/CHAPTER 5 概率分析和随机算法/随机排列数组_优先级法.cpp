525809d628c7bdc3ae1a652c6410bf335875aa26
Jeza <jeza@vip.qq.com>
2017-09-27 23:31:24 +0800

    20170927#随机排列数列的优先级排列方法

---
+++ Introduction to Algorithms 算法导论/CHAPTER 5 概率分析和随机算法/随机排列数组_优先级法.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+
+using namespace std;
+
+typedef struct
+{
+    int key;
+    int value;
+} Elem;
+
+bool sortUp (const Elem& a, const Elem& b)
+{
+    return a.key < b.key ;
+}
+void pemute (vector <Elem>& V)
+{
+    int n=V.size();
+    srand(time(NULL));
+    for(int i=0; i<n; i++)
+    {
+        V[i].key = rand() % (n * n * n);
+    }
+    sort(V.begin(), V.end(), sortUp);
+}
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        vector<Elem> V;
+        int r;
+        for(int i=0; i<n; i++)
+        {
+            Elem r;
+            cin>>r.value;
+            V.push_back(r);
+        }
+        pemute(V);
+        for(int i=0; i<V.size(); i++)
+            cout<<V[i].value<<" ";
+    }
+
+}
